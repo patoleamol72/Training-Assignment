70cded7013431af4bd8d4c8633714088b7c174c7
patoleamol72 <38864176+patoleamol72@users.noreply.github.com>
2018-06-01 19:38:18 +0530

    fourth commit

--- Projects/PEM Application/PersonalExpenseManager/src/in/infotech/PEMService.java
+++ Projects/PEM Application/PersonalExpenseManager/src/in/infotech/PEMService.java
@@ -1,5 +1,6 @@
 package in.infotech;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -17,17 +18,42 @@ public class PEMService {
 			printMenu();
 			switch(choice){
 			case 1:
-				//Add category
-				System.out.println("Add category");
+				onAddCategory();
+				pressAnyKeyToContine();
+				break;
+			case 2:
+				onCategoryList();
+				pressAnyKeyToContine();
+				break;
+			case 3:
+				onExpenseEntry();
+				pressAnyKeyToContine();
+				break;
+			case 4:
+				onExpenseList();
+				pressAnyKeyToContine();
+				break;
+			case 5:
+				onMonthlyExpenseList();
+				pressAnyKeyToContine();
+				break;
+			case 6:
+				onYearlyExpenseList();
+				pressAnyKeyToContine();
+				break;
+			case 7:
+				onCategorizedExpenseList();
+				pressAnyKeyToContine();
 				break;
 			case 0:
-				System.exit(0);
+				onExit();
 				break;
 				
 			}
 		}
 	}
 
+	
 	public void printMenu(){
 		System.out.println("-----PEM Menu------");
 		System.out.println("1. Add category");
@@ -42,4 +68,65 @@ public class PEMService {
 		System.out.print("Enter your choice");
 		choice = sc.nextInt();
 	}
+	public void pressAnyKeyToContine(){
+		System.out.println("Press any key to continue....");
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void onAddCategory() {
+		System.out.println("Adding category....");
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void onCategoryList() {
+		System.out.println("Listing categories...");
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void onExpenseEntry() {
+		System.out.println("Expense Entry.....");
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void onExpenseList() {
+		System.out.println("Expense list.......");
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void onMonthlyExpenseList() {
+		System.out.println("Monthly Expense Listing..");
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void onYearlyExpenseList() {
+		System.out.println("Yearly Expense Listing..");
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void onCategorizedExpenseList() {
+		System.out.println("Category wise Expense Listing..");
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void onExit() {
+		System.exit(0);
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
 }
